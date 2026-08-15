c5f0364659a7c36a595165ca3d43ebe33a2424e9
BIOUMKAMIROU <98597234+kamiroubioum80@users.noreply.github.com>
2026-08-15 18:36:39 +0100

    Update vercel.json

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const express = require("express");
-const app = express();
 
-app.use(express.json());
-app.use(express.static("."));
-
-app.post("/approve",(req,res)=>{
-console.log("Paiement approuvé :",req.body.paymentId);
-res.send({result:"ok"});
-});
-
-app.post("/complete",(req,res)=>{
-console.log("Paiement terminé :",req.body.paymentId);
-res.send({result:"ok"});
-});
-
-app.listen(3000,()=>{
-console.log("Serveur lancé");
-});
+organic-agropastoral/
+├── index.html          ← ton site
+├── README.md
+├── validation-key.txt  ← pour Pi Network
+└── vercel.json         ← pour dire à Vercel que c'est du HTML
